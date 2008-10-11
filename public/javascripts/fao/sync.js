de96796525dd8c241808548e37a2c787f782c7c4
jianglibo <jianglibo@gmail.com>
2008-10-12 00:59:04 +0800

    hanzi insert turn to google workpool

--- public/javascripts/fao/sync.js
+++ public/javascripts/fao/sync.js
@@ -2,12 +2,20 @@ fao.classes.MySync = function(){
   this.workerPool = google.gears.factory.create('beta.workerpool');
   this.workerPool.onmessage = function(a, b, message) {
 //    alert('Received message from worker ' + message.sender + ': \n' + message.body);
-    fao.doms.textout.innerHTML = message.body.text;
-
+    switch(message.body[2].action){
+      case 'popup':
+        alert(message.body[2].text);
+        break;
+      default:
+        fao.doms.textout.innerHTML = message.body[2].text;
+    }
   };
-  this.syncDataWorkerId = this.workerPool.createWorkerFromUrl('/javascripts/fao/sync_data_worker.js');
-  this.insertpyWorkerId = this.workerPool.createWorkerFromUrl('/javascripts/fao/insert_pys.js');
-  this.workerPool.sendMessage(["3..2..", 1, {}],this.insertpyWorkerId);
-  this.authenticityWorkerId = this.workerPool.createWorkerFromUrl('/javascripts/fao/authenticity_token_worker.js');
-  this.workerPool.sendMessage(["3..2..", 1, {syncWorkerId: this.syncDataWorkerId}], this.authenticityWorkerId);
+  if(!fao.variables.pinyindb){
+    this.insertpyWorkerId = this.workerPool.createWorkerFromUrl('/javascripts/fao/insert_pys.js');
+    this.workerPool.sendMessage(["3..2..", 1, {}],this.insertpyWorkerId);
+  }
+
+//  this.syncDataWorkerId = this.workerPool.createWorkerFromUrl('/javascripts/fao/sync_data_worker.js');
+//  this.authenticityWorkerId = this.workerPool.createWorkerFromUrl('/javascripts/fao/authenticity_token_worker.js');
+//  this.workerPool.sendMessage(["3..2..", 1, {syncWorkerId: this.syncDataWorkerId}], this.authenticityWorkerId);
 };
