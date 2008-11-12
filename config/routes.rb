719a533844356ccba29efd9c189e6a45b91e2b18
jianglibo <jianglibo@gmail.com>
2008-11-13 07:00:26 +0800

    'javascript'

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :activities,:has_many=>[:staffds]
 
-  map.resources :staffs,:has_many=>[:staffds]
+  map.resources :staffs,:has_many=>[:staffds],:member=>{:syncreate=>:post}
 
   map.resources :ceshi,:collection=>{:formauthenticitytoken=>:get,:testpost=>:post}
 
